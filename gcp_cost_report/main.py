5ac37289921126a42f11fb976e97674cafa96f90
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-03-08 15:16:18 +1100

    Print totals. (#7)
    Also add linters.

--- gcp_cost_report/main.py
+++ gcp_cost_report/main.py
@@ -1,9 +1,8 @@
 """A Cloud Function to send a daily GCP cost report to Slack."""
 
-import base64
-import json
 import logging
 import os
+from collections import defaultdict
 
 from google.cloud import bigquery
 from google.cloud import secretmanager
@@ -11,9 +10,9 @@ from google.cloud import secretmanager
 import slack
 from slack.errors import SlackApiError
 
-PROJECT_ID = os.getenv("GCP_PROJECT")
-BIGQUERY_BILLING_TABLE = os.getenv("BIGQUERY_BILLING_TABLE")
-QUERY_TIME_ZONE = os.getenv("QUERY_TIME_ZONE") or "UTC"
+PROJECT_ID = os.getenv('GCP_PROJECT')
+BIGQUERY_BILLING_TABLE = os.getenv('BIGQUERY_BILLING_TABLE')
+QUERY_TIME_ZONE = os.getenv('QUERY_TIME_ZONE') or 'UTC'
 
 # Query monthly cost per project and join that with cost over the last day.
 BIGQUERY_QUERY = f"""
@@ -67,43 +66,57 @@ ORDER BY
 
 SLACK_CHANNEL = os.getenv('SLACK_CHANNEL')
 SLACK_TOKEN_SECRET_NAME = (
-    f'projects/{PROJECT_ID}/secrets/slack-gcp-cost-control/versions/latest')
+    f'projects/{PROJECT_ID}/secrets/slack-gcp-cost-control/versions/latest'
+)
 
 # Cache the Slack client.
 secret_manager = secretmanager.SecretManagerServiceClient()
 slack_token_response = secret_manager.access_secret_version(
-    request={"name": SLACK_TOKEN_SECRET_NAME})
+    request={'name': SLACK_TOKEN_SECRET_NAME}
+)
 slack_token = slack_token_response.payload.data.decode('UTF-8')
 slack_client = slack.WebClient(token=slack_token)
 
 bigquery_client = bigquery.Client()
 
 
-def gcp_cost_report(data, unused_context):
+def gcp_cost_report(unused_data, unused_context):
     """Main entry point for the Cloud Function."""
 
+    totals = defaultdict(lambda: defaultdict(float))
     lines = []
     for row in bigquery_client.query(BIGQUERY_QUERY):
-        line = (f"*{row['project_id']}:* "
-                f"this month: {row['month']} {row['currency']}")
-        if (row['day']):
-            line += f", last 24h: {row['day']} {row['currency']}"
+        line = (
+            f'*{row["project_id"]}:* ' f'this month: {row["month"]} {row["currency"]}'
+        )
+        totals[row['currency']]['month'] += row['month']
+        if row['day']:
+            line += f', last 24h: {row["day"]} {row["currency"]}'
+            totals[row['currency']]['day'] += row['day']
         lines.append(line)
 
-    if len(lines):
-        post_slack_message('\n'.join(lines))
+    total_lines = []
+    for currency, vals in totals.items():
+        total_lines.append(
+            f'_*All projects:*_ this month: '
+            f'{round(vals["month"], 2)} {currency}, '
+            f'last 24h: {round(vals["day"], 2)} {currency}'
+        )
+
+    if lines:
+        post_slack_message('\n'.join(total_lines + lines))
 
 
 def post_slack_message(text):
     """Posts the given text as message to Slack."""
 
     try:
-        slack_client.api_call(
+        slack_client.api_call(  # pylint: disable=duplicate-code
             'chat.postMessage',
             json={
                 'channel': SLACK_CHANNEL,
                 'text': text,
-            }
+            },
         )
     except SlackApiError as err:
-        logging.error('Error posting to Slack: %s', err)
+        logging.error(f'Error posting to Slack: {err}')
