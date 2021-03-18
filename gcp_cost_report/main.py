3db3a2ba3d706bd5500b26b769b21a7f84e71ff0
Michael Franklin <michael@illusional.net>
2021-03-18 18:00:17 +1100

    Add budget percentage used to GCP cost control (#8)
    * Add google-cloud-billing-projects
    
    * Add initial implementation
    
    * Minor code quality improvement
    
    * Use BUDGET_ACCOUNT_ID environment variable.
    
    * Change budget index to attribute selection
    
    * Fix google Budget attribute names
    
    * Fix inner specified amount
    
    * Cast to string before joining
    
    * Print slack blocks for debugging
    
    * json.dumps slack blocks
    
    * Add print statement to slack debug
    
    * Use newlines to separate blocks + minor fixes
    
    * Use tabulate to generate monospaced table
    
    * Make projects >80% budget used bold
    
    * Add log if percent_used_str can't be determined
    
    * Ensure monthly value is passed as float to calculate percent
    
    * Restructure output format
    
    * Use :.2f formatter for currency
    
    * Remove stale TODO
    
    * Remove stale TODO
    
    * Apply suggestions from code review
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

--- gcp_cost_report/main.py
+++ gcp_cost_report/main.py
@@ -1,14 +1,19 @@
 """A Cloud Function to send a daily GCP cost report to Slack."""
 
+# import json
+import json
 import logging
 import os
 from collections import defaultdict
+from typing import Tuple, List
 
 from google.cloud import bigquery
 from google.cloud import secretmanager
+import google.cloud.billing.budgets_v1.services.budget_service as budget
 
 import slack
 from slack.errors import SlackApiError
+
 
 PROJECT_ID = os.getenv('GCP_PROJECT')
 BIGQUERY_BILLING_TABLE = os.getenv('BIGQUERY_BILLING_TABLE')
@@ -68,6 +73,7 @@ SLACK_CHANNEL = os.getenv('SLACK_CHANNEL')
 SLACK_TOKEN_SECRET_NAME = (
     f'projects/{PROJECT_ID}/secrets/slack-gcp-cost-control/versions/latest'
 )
+BILLING_ACCOUNT_ID = os.getenv('BILLING_ACCOUNT_ID')
 
 # Cache the Slack client.
 secret_manager = secretmanager.SecretManagerServiceClient()
@@ -78,44 +84,144 @@ slack_token = slack_token_response.payload.data.decode('UTF-8')
 slack_client = slack.WebClient(token=slack_token)
 
 bigquery_client = bigquery.Client()
+budget_client = budget.BudgetServiceClient()
+
+
+def try_cast_int(i):
+    """Cast i to int, else return None if ValueError"""
+    try:
+        return int(i)
+    except ValueError:
+        return None
+
+
+def try_cast_float(f):
+    """Cast i to float, else return None if ValueError"""
+    try:
+        return float(f)
+    except ValueError:
+        return None
 
 
 def gcp_cost_report(unused_data, unused_context):
     """Main entry point for the Cloud Function."""
 
     totals = defaultdict(lambda: defaultdict(float))
-    lines = []
+    budgets = budget_client.list_budgets(parent=f'billingAccounts/{BILLING_ACCOUNT_ID}')
+    budgets_map = {b.display_name: b for b in budgets}
+
+    def add_currency_to_non_null_fields(fields, currency, separator=' | '):
+        return separator.join(f'{field} {currency}' for field in fields if field is not None)
+
+    summary_header = ('Project', '24h | month (% used)')
+    project_summary: List[Tuple[str, str]] = []
+    totals_summary: List[Tuple[str, str]] = []
+
     for row in bigquery_client.query(BIGQUERY_QUERY):
-        line = (
-            f'*{row["project_id"]}:* ' f'this month: {row["month"]} {row["currency"]}'
-        )
-        totals[row['currency']]['month'] += row['month']
+        project_id = row['project_id']
+        currency = row['currency']
+        last_month = row['month']
+        last_month_str = f'{last_month:.2f}'
+        last_day_str = None
+        percent_used = None
+
         if row['day']:
-            line += f', last 24h: {row["day"]} {row["currency"]}'
-            totals[row['currency']]['day'] += row['day']
-        lines.append(line)
+            last_day_str = f'{row["day"]:.2f}'
+            totals[currency]['day'] += row['day']
 
-    total_lines = []
+        row_str = add_currency_to_non_null_els([last_day_str, last_month_str], currency)
+
+        if project_id in budgets_map:
+            percent_used, percent_used_str = get_percent_used_from_budget(
+                budgets_map[project_id],
+                last_month,
+                currency,
+            )
+            if percent_used_str:
+                row_str += f' ({percent_used_str})'
+
+        else:
+            logging.warning(
+                f"Couldn't find project_id {project_id} in "
+                f"budgets: {', '.join(budgets_map.keys())}"
+            )
+
+        # potential formatting
+        if percent_used is not None:
+            if percent_used >= 0.8:
+                # make fields bold
+                project_id = f'*{project_id}*'
+                row_str = f'*{row_str}*'
+
+        project_summary.append((project_id, row_str))
+
     for currency, vals in totals.items():
-        total_lines.append(
-            f'_*All projects:*_ this month: '
-            f'{round(vals["month"], 2)} {currency}, '
-            f'last 24h: {round(vals["day"], 2)} {currency}'
+        last_day_str = f'{vals["day"]:.2f}'
+        last_month_str = f'{vals["month"]:.2f}'
+
+        # totals don't have percent used
+        totals_summary.append(
+            (
+                '_All projects:_',
+                add_currency_to_non_null_els([last_day_str, last_month], currency),
+            )
         )
 
-    if lines:
-        post_slack_message('\n'.join(total_lines + lines))
+        all_rows = [summary_header, *totals_summary, *project_summary]
+        if len(all_rows) > 1:
+
+            def wrap_in_mrkdwn(a):
+                return {'type': 'mrkdwn', 'text': a}
+
+            body = [
+                wrap_in_mrkdwn('\n'.join(a[0] for a in all_rows)),
+                wrap_in_mrkdwn('\n'.join(a[1] for a in all_rows)),
+            ]
+            blocks = [{'type': 'section', 'fields': body}]
+            post_slack_message(blocks=blocks)
 
 
-def post_slack_message(text):
+def get_percent_used_from_budget(b, last_month_total, currency) -> Tuple[float, str]:
+    """Get percent_used as a string from GCP billing budget"""
+    percent_used = None
+    percent_used_str = ''
+    inner_amount = b.amount.specified_amount
+    if not inner_amount:
+        return None, ''
+    budget_currency = inner_amount.currency_code
+
+    # 'units' is an int64, which is represented as a string in JSON,
+    # this can be safely stored in Python3: https://stackoverflow.com/a/46699498
+    budget_total = try_cast_int(inner_amount.units)
+    monthly_used_float = try_cast_float(last_month_total)
+
+    if budget_total and monthly_used_float:
+        percent_used = monthly_used_float / budget_total
+        percent_used_str = f'{round(percent_used * 100)}%'
+        if budget_currency != currency:
+            # there's a currency mismatch
+            percent_used_str += (
+                f' (mismatch currency, budget: {budget_currency} | total: {currency})'
+            )
+
+    else:
+        logging.warning(
+            "Couldn't determine the budget amount from the budget, "
+            f'inner_amount.units: {inner_amount.units}, '
+            f'monthly_used_float: {monthly_used_float}'
+        )
+
+    return percent_used, percent_used_str
+
+
+def post_slack_message(blocks):
     """Posts the given text as message to Slack."""
-
     try:
         slack_client.api_call(  # pylint: disable=duplicate-code
             'chat.postMessage',
             json={
                 'channel': SLACK_CHANNEL,
-                'text': text,
+                'blocks': json.dumps(blocks),
             },
         )
     except SlackApiError as err:
