8ea65307228e42930c08f4ef7bec047ca7ed938e
Sabrina Yan <sabrina.yan@populationgenomics.org.au>
2022-04-07 17:35:34 +1000

    START: start on gcp_billing cloud function to move to aggregate bq table

--- aggregate/gcp_billing/main.py
+++ aggregate/gcp_billing/main.py
@@ -12,16 +12,75 @@ Tasks:
 - Service ID should be faithfully handed over
 """
 
+import json
 
-def main(request):
+from google.cloud import bigquery
 
-    start_period = None
-    finish_period = None
-    pass
+from cpg_utils.cloud import read_secret
+
+
+ANALYSIS_RUNNER_PROJECT_ID = 'analysis-runner'
+SOURCE_TABLE = 'billing-admin-290403.billing.gcp_billing_export_v1_01D012_20A6A2_CBD343'
+DESTINATION_TABLE = 'sabrina-dev-337923.billing.aggregate'
+
+bigquery_client = bigquery.Client()
+
+
+def main():
+    """Main entry point for the Cloud Function"""
+    start_period = "2022-04-05"
+    finish_period = "2022-04-05"
+
+    # Get the dataset to GCP project map
+    dataset_to_gcp_map = get_dataset_to_gcp_map()
+    allowed_project_ids = "'" + "','".join(dataset_to_gcp_map.values()) + "'"
+
+    # Get the billing date in the time period
+    # Filter out any rows that aren't in the allowed project ids
+    _query = f"""
+        SELECT * FROM `{SOURCE_TABLE}`
+        WHERE DATE(usage_end_time) >= '{start_period}'
+            AND DATE(usage_end_time) <= '{finish_period}'
+            AND project.id IN ({allowed_project_ids})
+    """
+
+    migrate_rows = bigquery_client.query(_query).result().to_dataframe()
+    migrate_rows['id'] = migrate_rows.apply(billing_row_to_key, axis=1)
+    migrate_rows['dataset'] = migrate_rows.apply(
+        billing_row_to_dataset, axis=1, args=(dataset_to_gcp_map,)
+    )
+
+    # Merge the rows with the destination table
+
+    migrate_rows.to_gbq(DESTINATION_TABLE, if_exists='merge')
+
+    return
+
+
+def billing_row_to_key(row):
+    """Convert a billing row to a key"""
+    """
+        service.id, usage_start_time, usage_end_time, labels.bucket, usage[0].amount,
+    """
+    return (
+        row['service']['id'],
+        row['usage_start_time'],
+        row['usage_end_time'],
+        row['labels'][0].get('bucket', None) if row['labels'].any() else None,
+        row['usage']['amount'],
+    )
+
+
+def billing_row_to_dataset(row, dataset_to_gcp_map):
+    """Convert a billing row to a dataset"""
+    return dataset_to_gcp_map.get(row['project']['id'], None)
 
 
 def get_dataset_to_gcp_map() -> dict:
     """Get the server-config from the secret manager"""
-    m = json.loads(read_secret(ANALYSIS_RUNNER_PROJECT_ID, 'server-config'))
+    server_config = json.loads(read_secret(ANALYSIS_RUNNER_PROJECT_ID, 'server-config'))
+    return {k: v['projectId'] for k, v in server_config.items()}
 
-    return {k: v['projectId'] for k, v in m.items()}
+
+if __name__ == '__main__':
+    main()
