20d0f796ac7deff55adb6d0d8a2d5b48b7c04c4b
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-05-09 12:44:24 +1000

    Refactor to use json dump for BQ data

--- aggregate/billing_functions/hail.py
+++ aggregate/billing_functions/hail.py
@@ -30,40 +30,35 @@ Tasks:
     - And maybe only sync "settled" jobs within datetimes
         (ie: finished between START + END of previous time period)
 """
-
+import logging
+import json
 from collections import defaultdict
 from datetime import datetime, timedelta
-import os
-import json
-import logging
 from typing import Dict, List
-import requests
-from google.cloud import bigquery
 
-import pandas as pd
+from cpg_utils.cloud import read_secret
 
-from utils import insert_new_rows_in_table
-
-
-GCP_BILLING_BQ_TABLE = (
-    'billing-admin-290403.billing.gcp_billing_export_v1_01D012_20A6A2_CBD343'
+from utils import (
+    insert_new_rows_in_table,
+    get_currency_conversion_rate_for_time,
+    get_unit_for_batch_resource_type,
+    get_usd_cost_for_resource,
+    get_finished_batches_for_date,
+    get_hail_token,
+    fill_batch_jobs,
+    parse_hail_time,
+    to_bq_time,
+    HAIL_UI_URL,
+    SERVICE_FEE,
+    ANALYSIS_RUNNER_PROJECT_ID,
 )
+
 GCP_DEST_TABLE = 'billing-admin-290403.billing_aggregate.michael-dev'
-bigquery_client = bigquery.Client()
 
 
 logging.basicConfig(level=logging.INFO)
 
 SERVICE_ID = 'hail'
-
-BASE = 'https://batch.hail.populationgenomics.org.au'
-UI_URL = BASE + '/batches/{batch_id}'
-BATCHES_API = BASE + '/api/v1alpha/batches'
-JOBS_API = BASE + '/api/v1alpha/batches/{batch_id}/jobs/resources'
-
-
-def parse_hail_time(time_str: str) -> datetime:
-    return datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%SZ')
 
 
 def get_billing_projects():
@@ -71,170 +66,11 @@ def get_billing_projects():
     Get Hail billing projects, same names as dataset
     TOOD: Implement
     """
-    return ['seqr']
 
-
-def get_hail_token():
-    """
-    Get Hail token from local tokens file
-    TODO: look at env var for deploy
-    """
-    with open(os.path.expanduser('~/.hail/tokens.json')) as f:
-        config = json.load(f)
-        return config['default']
-
-
-def get_batches(billing_project: str, last_batch_id: any, token: str):
-    """
-    Get list of batches for a billing project with no filtering.
-    (Optional): from last_batch_id
-    """
-    q = f'?q=billing_project:{billing_project}'
-    if last_batch_id:
-        q += f'&last_batch_id={last_batch_id}'
-
-    response = requests.get(
-        BATCHES_API + q, headers={'Authorization': 'Bearer ' + token}
-    )
-    response.raise_for_status()
-
-    return response.json()
-
-
-def get_finished_batches_for_date(
-    billing_project: str, start_day: datetime, end_day: datetime, token: str
-):
-    """
-    Get all the batches that started on {date} and are complete.
-    We assume that batches are ordered by start time, so we can stop
-    when we find a batch that started before the date.
-    """
-    batches = []
-    last_batch_id = None
-    n_requests = 0
-    skipped = 0
-
-    while True:
-
-        n_requests += 1
-        jresponse = get_batches(billing_project, last_batch_id, token)
-
-        if 'last_batch_id' in jresponse and jresponse['last_batch_id'] == last_batch_id:
-            raise ValueError(
-                f'Something weird is happening with last_batch_job: {last_batch_id}'
-            )
-        last_batch_id = jresponse['last_batch_id']
-        for b in jresponse['batches']:
-            if not b['time_completed'] or not b['complete']:
-                skipped += 1
-                continue
-
-            time_completed = parse_hail_time(b['time_completed'])
-            in_date_range = time_completed >= start_day and time_completed < end_day
-
-            if time_completed < start_day:
-                logging.info(
-                    f'Got batches in {n_requests} requests, skipping {skipped}'
-                )
-                return batches
-            if in_date_range:
-                batches.append(b)
-            else:
-                skipped += 1
-
-
-def fill_batch_jobs(batch: dict, token: str):
-    """
-    For a single batch, fill in the "jobs" field.
-
-    TODO: use new endpoint with billing info
-    """
-    batch['jobs'] = []
-    last_job_id = None
-    end = False
-    iterations = 0
-    while not end:
-        iterations += 1
-
-        if iterations > 1 and iterations % 5 == 0:
-            logging.info(f'On {iterations} iteration to load jobs for {batch["id"]}')
-
-        q = ''
-        if last_job_id:
-            q = f'?last_job_id={last_job_id}'
-        url = JOBS_API.format(batch_id=batch['id']) + q
-        response = requests.get(url, headers={'Authorization': 'Bearer ' + token})
-        response.raise_for_status()
-
-        jresponse = response.json()
-        new_last_job_id = jresponse.get('last_job_id')
-        if new_last_job_id is None:
-            end = True
-        elif last_job_id and new_last_job_id <= last_job_id:
-            raise ValueError('Something fishy with last job id')
-        last_job_id = new_last_job_id
-        batch['jobs'].extend(jresponse['jobs'])
-
-    return batch
-
-
-CACHED_CURRENCY_CONVERSION: Dict[str, float] = {}
-
-
-def get_currency_conversion_rate_for_time(time: datetime):
-    """
-    Get the currency conversion rate for a given time.
-    Noting that GCP conversion rates are decided at the start of the month,
-    and apply to each job that starts within the month, regardless of when
-    the job finishes.
-    """
-    global CACHED_CURRENCY_CONVERSION
-
-    key = str(time.date())
-    if key not in CACHED_CURRENCY_CONVERSION:
-        logging.warn(f'Looking up currency conversion rate for {key}')
-        query = f"""
-            SELECT currency_conversion_rate
-            FROM {GCP_BILLING_BQ_TABLE}
-            WHERE DATE(_PARTITIONTIME) = DATE('{time.date()}')
-            LIMIT 1
-        """
-        for r in bigquery_client.query(query).result():
-            CACHED_CURRENCY_CONVERSION[key] = r['currency_conversion_rate']
-
-    return CACHED_CURRENCY_CONVERSION[key]
-
-
-def get_usd_cost_for_resource(batch_resource, usage):
-    """
-    Get the cost of a resource in USD.
-    """
-    # TODO: fix these costs, they're the ones from hail directory
-    return {
-        "boot-disk/pd-ssd/1": 0.0000000000000631286124420108,
-        "compute/n1-nonpreemptible/1": 0.00000000000878083333333334,
-        "compute/n1-preemptible/1": 0.00000000000184861111111111,
-        "disk/local-ssd/1": 0.0000000000000178245493953913,
-        "disk/pd-ssd/1": 0.0000000000000631286124420108,
-        "ip-fee/1024/1": 0.00000000000108506944444444,
-        "memory/n1-nonpreemptible/1": 0.00000000000114935980902778,
-        "memory/n1-preemptible/1": 0.000000000000241970486111111,
-        "service-fee/1": 0.00000000000277777777777778,
-    }[batch_resource] * usage
-
-
-def get_unit_for_batch_resource_type(batch_resource_type: str) -> str:
-    return {
-        "boot-disk/pd-ssd/1": "GB/ms",
-        "disk/local-ssd/1": "GB/ms",
-        "disk/pd-ssd/1": "GB/ms",
-        "compute/n1-nonpreemptible/1": "cpu/ms",
-        "compute/n1-preemptible/1": 'cpu/ms',
-        "ip-fee/1024/1": 'IPs/ms',
-        "memory/n1-nonpreemptible/1": 'GB/ms',
-        "memory/n1-preemptible/1": 'GB/ms',
-        "service-fee/1": '$/ms',
-    }.get(batch_resource_type, batch_resource_type)
+    server_config = json.loads(read_secret(ANALYSIS_RUNNER_PROJECT_ID, 'server-config'))
+    datasets_to_ignore = {'hail', 'seqr'}
+    ds = list(set(server_config.keys()) - datasets_to_ignore)
+    return ds
 
 
 def get_finalised_entries_for_batch(dataset, batch: dict) -> List[Dict]:
@@ -254,19 +90,25 @@ def get_finalised_entries_for_batch(dataset, batch: dict) -> List[Dict]:
             resource_usage[batch_resource] += usage
 
     for batch_resource, usage in resource_usage.items():
-        key = f'hail-{dataset}-batch-{batch["id"]}'
+        key = f'hail-{dataset}-batch-{batch["id"]}-{batch_resource}'
 
         labels = [
-            {'name': 'dataset', 'value': dataset},
-            {'name': 'url', 'value': UI_URL.replace('{batch_id}', str(batch['id']))},
+            {'key': 'dataset', 'value': dataset},
+            {
+                'key': 'url',
+                'value': HAIL_UI_URL.replace('{batch_id}', str(batch['id'])),
+            },
         ]
         name = batch.get('attributes', {}).get('name')
         if name:
-            labels.append({'name': 'name', 'value': name})
-        # batch_resource is one of ['cpu', 'memory', 'disk']
+            labels.append(
+                {'key': 'name', 'value': name.encode("ascii", "ignore").decode()}
+            )
 
-        cost = currency_conversion_rate * get_usd_cost_for_resource(
-            batch_resource, usage
+        cost = (
+            (1 + SERVICE_FEE)
+            * currency_conversion_rate
+            * get_usd_cost_for_resource(batch_resource, usage)
         )
 
         entries.append(
@@ -275,14 +117,14 @@ def get_finalised_entries_for_batch(dataset, batch: dict) -> List[Dict]:
                 "dataset": dataset,
                 "service": {
                     "id": key,
-                    "description": 'Hail compute',
+                    "description": 'Hail compute: ' + batch_resource,
                 },
                 "sku": {
                     "id": f'hail-{batch_resource}',
                     "description": f'{batch_resource} usage',
                 },
-                "usage_start_time": start_time,
-                "usage_end_time": end_time,
+                "usage_start_time": to_bq_time(start_time),
+                "usage_end_time": to_bq_time(end_time),
                 'project': None,
                 "labels": labels,
                 "system_labels": [],
@@ -292,7 +134,7 @@ def get_finalised_entries_for_batch(dataset, batch: dict) -> List[Dict]:
                     "region": 'australia',
                     "zone": None,
                 },
-                "export_time": datetime.now(),
+                "export_time": to_bq_time(datetime.now()),
                 "cost": cost,
                 "currency": "AUD",
                 "currency_conversion_rate": currency_conversion_rate,
@@ -302,7 +144,7 @@ def get_finalised_entries_for_batch(dataset, batch: dict) -> List[Dict]:
                     "amount_in_pricing_units": cost,
                     "pricing_unit": "AUD",
                 },
-                # 'credits': [],
+                'credits': [],
                 "invoice": {"month": f'{start_time.month}{start_time.year}'},
                 "cost_type": "regular",
                 'adjustment_info': None,
@@ -331,18 +173,17 @@ def main(start: datetime = None, end: datetime = None):
     token = get_hail_token()
     entries = []
     for bp in get_billing_projects():
+        logging.info(f'{bp} :: loading batches')
         batches = get_finished_batches_for_date(bp, start, end, token)
-        batches = [b for b in batches if '...' not in b['attributes']['name']]
+        # batches = [b for b in batches if '...' not in b['attributes']['name']]
         logging.info(f'{bp} :: Filling in information for {len(batches)} batches')
 
         for batch in batches:
             fill_batch_jobs(batch, token)
             entries.extend(get_finalised_entries_for_batch(bp, batch))
 
-    df = pd.DataFrame.from_records(entries)
-
     # Insert new rows into aggregation table
-    insert_new_rows_in_table(client=bigquery_client, table=GCP_DEST_TABLE, df=df)
+    insert_new_rows_in_table(table=GCP_DEST_TABLE, obj=entries)
 
 
 if __name__ == '__main__':
--- aggregate/billing_functions/utils.py
+++ aggregate/billing_functions/utils.py
@@ -1,21 +1,39 @@
+import os
+from collections import defaultdict
 import json
 import logging
-import pandas as pd
 
-from typing import Tuple
+from typing import Dict, List, Any
 from pathlib import Path
+
+import requests
+
 from datetime import datetime
-from google.cloud.bigquery import (
-    Client,
-    QueryJobConfig,
-    ArrayQueryParameter,
-    ScalarQueryParameter,
-)
+import google.cloud.bigquery as bq
+from google.api_core.exceptions import BadRequest, ClientError
+from io import StringIO
 
 logging.basicConfig(level=logging.INFO)
 
+GCP_BILLING_BQ_TABLE = (
+    'billing-admin-290403.billing.gcp_billing_export_v1_01D012_20A6A2_CBD343'
+)
 
-def get_schema():
+# 10% overhead
+SERVICE_FEE = 0.1
+ANALYSIS_RUNNER_PROJECT_ID = 'analysis-runner'
+
+
+HAIL_BASE = 'https://batch.hail.populationgenomics.org.au'
+HAIL_UI_URL = HAIL_BASE + '/batches/{batch_id}'
+HAIL_BATCHES_API = HAIL_BASE + '/api/v1alpha/batches'
+HAIL_JOBS_API = HAIL_BASE + '/api/v1alpha/batches/{batch_id}/jobs/resources'
+
+
+bigquery_client = bq.Client()
+
+
+def get_schema_json():
     """Get the schema for the table"""
     pwd = Path(__file__).parent.parent.resolve()
     schema_path = pwd / 'schema' / 'aggregate_schema.json'
@@ -23,49 +41,250 @@ def get_schema():
         return json.load(f)
 
 
-def insert_new_rows_in_table(client: Client, table: str, df: pd.DataFrame):
+def format_schema(schema):
+    formatted_schema = []
+    for row in schema:
+        kwargs = {
+            'name': row['name'],
+            'field_type': row['type'],
+            'mode': row['mode'],
+        }
+
+        if 'fields' in row and row['fields']:
+            kwargs['fields'] = format_schema(row['fields'])
+        formatted_schema.append(bq.SchemaField(**kwargs))
+    return formatted_schema
+
+
+def get_formatted_schema():
+    return format_schema(get_schema_json())
+
+
+def parse_hail_time(time_str: str) -> datetime:
+    return datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%SZ')
+
+
+def to_bq_time(time: datetime):
+    return time.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def get_hail_token():
+    """
+    Get Hail token from local tokens file
+    TODO: look at env var for deploy
+    """
+    with open(os.path.expanduser('~/.hail/tokens.json')) as f:
+        config = json.load(f)
+        return config['default']
+
+
+def get_batches(billing_project: str, last_batch_id: any, token: str):
+    """
+    Get list of batches for a billing project with no filtering.
+    (Optional): from last_batch_id
+    """
+    q = f'?q=billing_project:{billing_project}'
+    if last_batch_id:
+        q += f'&last_batch_id={last_batch_id}'
+
+    response = requests.get(
+        HAIL_BATCHES_API + q, headers={'Authorization': 'Bearer ' + token}
+    )
+    response.raise_for_status()
+
+    return response.json()
+
+
+def get_finished_batches_for_date(
+    billing_project: str, start_day: datetime, end_day: datetime, token: str
+):
+    """
+    Get all the batches that started on {date} and are complete.
+    We assume that batches are ordered by start time, so we can stop
+    when we find a batch that started before the date.
+    """
+    batches = []
+    last_batch_id = None
+    n_requests = 0
+    skipped = 0
+
+    while True:
+
+        n_requests += 1
+        jresponse = get_batches(billing_project, last_batch_id, token)
+
+        if 'last_batch_id' in jresponse and jresponse['last_batch_id'] == last_batch_id:
+            raise ValueError(
+                f'Something weird is happening with last_batch_job: {last_batch_id}'
+            )
+        last_batch_id = jresponse.get('last_batch_id')
+        if not jresponse.get('batches'):
+            return batches
+        for b in jresponse['batches']:
+            if not b['time_completed'] or not b['complete']:
+                skipped += 1
+                continue
+
+            time_completed = parse_hail_time(b['time_completed'])
+            in_date_range = time_completed >= start_day and time_completed < end_day
+
+            if time_completed < start_day:
+                logging.info(
+                    f'Got batches in {n_requests} requests, skipping {skipped}'
+                )
+                return batches
+            if in_date_range:
+                batches.append(b)
+            else:
+                skipped += 1
+
+
+def fill_batch_jobs(batch: dict, token: str):
+    """
+    For a single batch, fill in the "jobs" field.
+
+    TODO: use new endpoint with billing info
+    """
+    batch['jobs'] = []
+    last_job_id = None
+    end = False
+    iterations = 0
+    while not end:
+        iterations += 1
+
+        if iterations > 1 and iterations % 5 == 0:
+            logging.info(f'On {iterations} iteration to load jobs for {batch["id"]}')
+
+        q = '?limit=9999'
+        if last_job_id:
+            q += f'&last_job_id={last_job_id}'
+        url = HAIL_JOBS_API.format(batch_id=batch['id']) + q
+        response = requests.get(url, headers={'Authorization': 'Bearer ' + token})
+        response.raise_for_status()
+
+        jresponse = response.json()
+        new_last_job_id = jresponse.get('last_job_id')
+        if new_last_job_id is None:
+            end = True
+        elif last_job_id and new_last_job_id <= last_job_id:
+            raise ValueError('Something fishy with last job id')
+        last_job_id = new_last_job_id
+        batch['jobs'].extend(jresponse['jobs'])
+
+    return batch
+
+
+def insert_new_rows_in_table(table: str, obj: List[Dict[str, Any]]):
     """Insert new rows into a table"""
 
     _query = f"""
         SELECT id FROM `{table}`
         WHERE id IN UNNEST(@ids);
     """
-    job_config = QueryJobConfig(
+    # ids = df['id']
+    ids = set(o['id'] for o in obj)
+    if len(ids) != len(obj):
+        counter = defaultdict(int)
+        for o in obj:
+            counter[o['id']] += 1
+        duplicates = [k for k, v in counter.items() if v > 1]
+        raise ValueError(
+            'There are multiple rows with the same id: ' + ', '.join(duplicates)
+        )
+
+    job_config = bq.QueryJobConfig(
         query_parameters=[
-            ArrayQueryParameter('ids', 'STRING', list(set(df['id']))),
+            bq.ArrayQueryParameter('ids', 'STRING', list(ids)),
         ]
     )
-    result = client.query(_query, job_config=job_config).result()
+
+    result = bigquery_client.query(_query, job_config=job_config).result()
     existing_ids = set(result.to_dataframe()['id'])
 
     # Filter out any rows that are already in the table
-    df = df[~df['id'].isin(existing_ids)]
+    filtered_obj = [o for o in obj if o['id'] not in existing_ids]
+
+    if len(filtered_obj) == 0:
+        logging.info('Not inserting any rows')
+        return []
 
     # Count number of rows adding
-    adding_rows = len(df)
+    logging.info(f"Inserting {len(filtered_obj)}/{len(obj)} rows")
 
     # Insert the new rows
-    project_id = table.split('.')[0]
+    job_config = bq.LoadJobConfig()
+    job_config.source_format = bq.SourceFormat.NEWLINE_DELIMITED_JSON
+    job_config.schema = get_formatted_schema()
 
-    # also doesn't work with failure:
-    #   BadRequest: 400 Error while reading data, error message:
-    #       Schema mismatch: referenced variable 'labels.$is_not_null' has array levels of 1,
-    #       while the corresponding field path to Parquet column has 0 repeated fields.
+    j = '\n'.join(json.dumps(o) for o in filtered_obj)
 
-    # result = client.load_table_from_dataframe(
-    #     df,
-    #     table,
-    # ).result()
-    # print(result)
-
-    table_schema = get_schema()
-
-    df.to_gbq(
-        table,
-        project_id=project_id,
-        table_schema=table_schema,
-        if_exists='append',
+    resp = bigquery_client.load_table_from_file(
+        StringIO(j), table, job_config=job_config
     )
+    try:
+        result = resp.result()
+        logging.info(str(result))
+    except ClientError as e:
+        logging.error(resp.errors)
+        raise e
 
-    logging.info(f"{adding_rows} new rows inserted")
-    return adding_rows
+    return filtered_obj
+
+
+CACHED_CURRENCY_CONVERSION: Dict[str, float] = {}
+
+
+def get_currency_conversion_rate_for_time(time: datetime):
+    """
+    Get the currency conversion rate for a given time.
+    Noting that GCP conversion rates are decided at the start of the month,
+    and apply to each job that starts within the month, regardless of when
+    the job finishes.
+    """
+    global CACHED_CURRENCY_CONVERSION
+
+    key = f'{time.year}-{time.month}'
+    if key not in CACHED_CURRENCY_CONVERSION:
+        logging.warn(f'Looking up currency conversion rate for {key}')
+        query = f"""
+            SELECT currency_conversion_rate
+            FROM {GCP_BILLING_BQ_TABLE}
+            WHERE DATE(_PARTITIONTIME) = DATE('{time.date()}')
+            LIMIT 1
+        """
+        for r in bigquery_client.query(query).result():
+            CACHED_CURRENCY_CONVERSION[key] = r['currency_conversion_rate']
+
+    return CACHED_CURRENCY_CONVERSION[key]
+
+
+def get_usd_cost_for_resource(batch_resource, usage):
+    """
+    Get the cost of a resource in USD.
+    """
+    # TODO: fix these costs, they're the ones from hail directory
+    return {
+        "boot-disk/pd-ssd/1": 0.0000000000000631286124420108,
+        "compute/n1-nonpreemptible/1": 0.00000000000878083333333334,
+        "compute/n1-preemptible/1": 0.00000000000184861111111111,
+        "disk/local-ssd/1": 0.0000000000000178245493953913,
+        "disk/pd-ssd/1": 0.0000000000000631286124420108,
+        "ip-fee/1024/1": 0.00000000000108506944444444,
+        "memory/n1-nonpreemptible/1": 0.00000000000114935980902778,
+        "memory/n1-preemptible/1": 0.000000000000241970486111111,
+        "service-fee/1": 0.00000000000277777777777778,
+    }[batch_resource] * usage
+
+
+def get_unit_for_batch_resource_type(batch_resource_type: str) -> str:
+    return {
+        "boot-disk/pd-ssd/1": "GB/ms",
+        "disk/local-ssd/1": "GB/ms",
+        "disk/pd-ssd/1": "GB/ms",
+        "compute/n1-nonpreemptible/1": "cpu/ms",
+        "compute/n1-preemptible/1": 'cpu/ms',
+        "ip-fee/1024/1": 'IPs/ms',
+        "memory/n1-nonpreemptible/1": 'GB/ms',
+        "memory/n1-preemptible/1": 'GB/ms',
+        "service-fee/1": '$/ms',
+    }.get(batch_resource_type, batch_resource_type)
