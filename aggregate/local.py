34162d17cd0ae91b204a38a2c9416665b3d7c6ca
Sabrina Yan <sabrina.yan@populationgenomics.org.au>
2022-05-18 09:13:35 +1000

    ADD: local run file

---
+++ aggregate/local.py
@@ -0,0 +1,11 @@
+import tracemalloc
+from billing_functions import gcp, hail, seqr
+
+args = {"attributes": {"start": "2022-04-01", "end": "2022-05-17"}}
+gcp(args)
+
+# tracemalloc.start()
+# gcp()
+# current, peak = tracemalloc.get_traced_memory()
+# print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+# tracemalloc.stop()
