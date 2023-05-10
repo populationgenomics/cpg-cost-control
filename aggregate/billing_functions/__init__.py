a3403e3bc2b58c2572ffe84bfa535bae65dc0543
Sabrina Yan <9669990+violetbrina@users.noreply.github.com>
2023-05-10 16:06:39 +1000

    REMOVE: old billing aggregate code (#39)

--- aggregate/billing_functions/__init__.py
+++
@@ -1,4 +0,0 @@
-# pylint: disable=unused-import,missing-module-docstring
-from .gcp import from_pubsub as gcp  # noqa: F401
-from .hail import from_pubsub as hail  # noqa: F401
-from .seqr import from_pubsub as seqr  # noqa: F401
--- aggregate/main.py
+++
@@ -1,3 +0,0 @@
-# pylint: disable=unused-import,missing-module-docstring
-from billing_functions import gcp, hail, seqr  # noqa: F401
-from monthly_upload import monthly_upload  # noqa: F401
