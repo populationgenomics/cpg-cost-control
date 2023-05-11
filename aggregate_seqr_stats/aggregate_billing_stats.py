d8ab693b7070943efebc5453edfb44ed525c7f36
Michael Franklin <michael.franklin@populationgenomics.org.au>
2023-05-11 13:43:00 +1000

    Better cost aggregation

--- aggregate_seqr_stats/aggregate_billing_stats.py
+++ aggregate_seqr_stats/aggregate_billing_stats.py
@@ -1,8 +1,9 @@
-import os.path
-
 import json
-import google.cloud.bigquery as bq
+import os.path
+import statistics
+from collections import defaultdict
 
+import google.cloud.bigquery as bq
 from sample_metadata.apis import ProjectApi
 
 _BQ_CLIENT = bq.Client()
@@ -61,21 +62,36 @@ def get_per_stage_data() -> dict:
         add_keys_value(by_seqtype_sample_stage_tool, keys, row['cost'])
 
     calculate_averages(by_seqtype_sample_stage_tool)
-    return by_seqtype_sample_stage_tool
+
+    with open('by-seqtype.json', 'w+') as f:
+        json.dump(by_seqtype_sample_stage_tool, f, sort_keys=True)
+
+    with open('by-seqtype-no-samples.json', 'w+') as f:
+        for sqt, stbody in by_seqtype_sample_stage_tool.items():
+            for stg, stgbody in stbody.items():
+                for tool, toolbody in stgbody.items():
+                    if not isinstance(toolbody, dict):
+                        continue
+                    stgbody[tool] = {
+                        k: toolbody[k] for k in toolbody if k.startswith('_')
+                    }
+        json.dump(by_seqtype_sample_stage_tool, f, sort_keys=True, indent=2)
 
 
 def calculate_averages(d):
 
     for seq_type, seq_body in d.items():
         for stage, stage_body in seq_body.items():
-            stage_average = 0
+            samples_costs = defaultdict(float)
             for tool, tool_body in stage_body.items():
                 tool_length = 0
-                tool_cost = 0
+                tool_cost = 0.0
                 for dataset_body in tool_body.values():
                     dataset_cost = sum(dataset_body.values())
                     tool_length += len(dataset_body)
                     tool_cost += dataset_cost
+                    for sample_id, sample_cost in dataset_body.items():
+                        samples_costs[sample_id] += sample_cost
                     dataset_body['_cost'] = dataset_cost
                     dataset_body['_length'] = len(dataset_body)
                     dataset_body['_average'] = dataset_cost / len(dataset_body)
@@ -85,9 +101,13 @@ def calculate_averages(d):
                     tool_body['_cost'] = tool_cost
                     tool_body['_average'] = tool_average
                     tool_body['_length'] = tool_length
-                    stage_average += tool_average
 
-            stage_body['_average'] = stage_average
+            raw_costs = list(samples_costs.values())
+            if len(raw_costs) > 1:
+                stage_body['_median'] = statistics.median(raw_costs)
+                stage_body['_length'] = len(raw_costs)
+                stage_body['_average'] = sum(raw_costs) / len(raw_costs)
+                stage_body['_stdev'] = statistics.stdev(raw_costs)
 
 
 def get_elasticsearch_cost() -> dict[str, float]:
@@ -111,8 +131,10 @@ def get_storage_cost():
     _query = """
     SELECT topic, invoice.month, SUM(cost)
     FROM `billing-admin-290403.billing_aggregate.aggregate`
-    WHERE service.description = 'Cloud Storage'
-    AND topic IN UNNEST(@topics)
+    WHERE
+        service.description = 'Cloud Storage'
+        AND topic IN UNNEST(@topics)
+        AND cost > 0
     GROUP BY topic, invoice.month
     """
     job_config = bq.QueryJobConfig(
@@ -120,9 +142,13 @@ def get_storage_cost():
             bq.ArrayQueryParameter('topics', 'STRING', seqr_project_names),
         ]
     )
-    df_bq_result = _BQ_CLIENT.query(_query, job_config=job_config).result()
-
-    print(df_bq_result)
+    _BQ_CLIENT.query(_query, job_config=job_config).result().to_dataframe().to_dict(
+        orient='records'
+    )
+    # for df_bq_result
+    #
+    # d = dict(df.values.tolist())
+    # return d
 
 
 def add_keys_value(d, keys, cost) -> None:
@@ -142,20 +168,8 @@ def add_keys_value(d, keys, cost) -> None:
 
 def main():
     # es_cost = get_elasticsearch_cost()
-    by_seqtype_sample_stage_tool = get_per_stage_data()
-    with open('by-seqtype.json', 'w+') as f:
-        json.dump(by_seqtype_sample_stage_tool, f, sort_keys=True)
-
-    with open('by-seqtype-no-samples.json', 'w+') as f:
-        for sqt, stbody in by_seqtype_sample_stage_tool.items():
-            for stg, stgbody in stbody.items():
-                for tool, toolbody in stgbody.items():
-                    if not isinstance(toolbody, dict):
-                        continue
-                    stgbody[tool] = {
-                        k: toolbody[k] for k in ['_average', '_length', '_cost']
-                    }
-        json.dump(by_seqtype_sample_stage_tool, f, sort_keys=True, indent=2)
+    # print(es_cost)
+    get_per_stage_data()
 
 
 if __name__ == '__main__':
